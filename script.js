40cb835e94b9f98b3ccff65bea554e0c27fc2c4a
Nauman Shabbir <mnaumanshabbir@gmail.com>
2023-05-09 11:47:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+turnOn = () =>{
+    document.getElementById('img').src='./Assets/images/pic_bulbon-on.gif'
+}
+turnOff =() =>{
+    document.getElementById('img').src='./Assets/images/pic_bulboff.gif'
+}
